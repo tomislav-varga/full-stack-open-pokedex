94b80c3859b76dc2e24735d1071fb1f829561246
tomi <tomislav.varga@icloud.com>
2025-04-03 16:07:04 +0200

    solution exercise 11.9 part 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,12 @@ module.exports = {
     "es6": true,
     "jest/globals": true,
     "node": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
@@ -17,7 +19,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
