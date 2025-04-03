e787f08f0676d9e1f5e6602957a34bb7306cd2c1
tomi <tomislav.varga@icloud.com>
2025-04-03 10:46:38 +0200

    solution exercise 11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true,
   },
   "extends": [
     "eslint:recommended",
@@ -29,7 +30,7 @@ module.exports = {
     ],
     "quotes": [
       "error",
-      "single"
+      "double"
     ],
     "semi": [
       "error",
